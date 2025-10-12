b5feadcd7c3174b3f9912bceea70fde92321eea6
Nikita <snitkonikita@gmail.com>
2025-10-13 00:43:29 +0200

    feat: add loaded logic for menu cards

--- src/js/menu.js
+++ src/js/menu.js
@@ -2,8 +2,11 @@ import { initMenu } from './utils/init.js';
 import { MenuCard } from './utils/cardMenu.js';
 import { Switchers } from './utils/switcherMenu.js';
 import { menuConfig } from './utils/config.js';
+import { getOrCreateLoaderBtn, handleResize } from './utils/helper.js';
 
 let products = [];
+const visibleCount = { count: 0 };
+const itemsPerPage = { items: window.innerWidth <= 768 ? 4 : 8 };
 const switcherSelector = '.menu__switchers__wrapper';
 const cardsSelector = '.menu__cards__wrapper';
 
@@ -12,19 +15,31 @@ export async function menu() {
   const switchersWrapper = document.querySelector(switcherSelector);
   const switchers = new Switchers(menuConfig, {
     default: 'coffee',
-    onChange: category => renderCards(category)
+    onChange: category => renderCards(category, true)
   });
   switchersWrapper.appendChild(switchers.createSwitchers());
 
-  renderCards(switchers.getActive());
+  renderCards(switchers.getActive(), true);
+
+  window.addEventListener('resize', () => {
+    handleResize(itemsPerPage, () => renderCards(switchers.getActive(), true));
+  });
 }
 
-function renderCards(category) {
+export function renderCards(category, reset = false) {
   const cardsWrapper = document.querySelector(cardsSelector);
   if (!cardsWrapper) return;
   cardsWrapper.classList.add('fade-out');
 
   setTimeout(() => {
+    if (reset) visibleCount.count = 0;
+
+    const filtered = products.filter(product => product.category === category);
+    const loaderBtn = getOrCreateLoaderBtn(cardsWrapper, category);
+
+    const maxToShow = Math.min(visibleCount.count + itemsPerPage.items, filtered.length);
+    const itemsToRender = filtered.slice(0, maxToShow);
+
     cardsWrapper.innerHTML = '';
 
     const categoryImageCounts = {
@@ -33,24 +48,29 @@ function renderCards(category) {
       tea: { count: 0, max: 4 }
     };
 
-    products
-      .filter(product => product.category === category)
-      .forEach(data => {
-        const { name, description, price, category, sizes, additives } = data;
+    itemsToRender.forEach(data => {
+      const { name, description, price, category, sizes, additives } = data;
+      if (!categoryImageCounts[category]) {
+        console.error(`This category is not found: ${category}`);
+        return;
+      }
 
-        if (!categoryImageCounts[category]) {
-          console.error(`This category is not found: ${category}`);
-          return;
-        }
+      categoryImageCounts[category].count = (categoryImageCounts[category].count % categoryImageCounts[category].max) + 1;
+      const categoryCount = categoryImageCounts[category].count;
+      const id = `${category}-${categoryImageCounts[category].count}`;
+      const image = `/images/${category}-${categoryCount}.png`;
+      const card = new MenuCard(category, sizes, additives, id, name, description, price, image);
+      const menuCard = card.createCards();
+      cardsWrapper.appendChild(menuCard);
+    });
 
-        categoryImageCounts[category].count = (categoryImageCounts[category].count % categoryImageCounts[category].max) + 1;
-        const categoryCount = categoryImageCounts[category].count;
-        const id = `${category}-${categoryImageCounts[category].count}`;
-        const image = `/images/${category}-${categoryCount}.png`;
-        const card = new MenuCard(category, sizes, additives, id, name, description, price, image);
-        const menuCard = card.createCards();
-        cardsWrapper.appendChild(menuCard);
-      });
+    visibleCount.count = maxToShow;
+
+    if (visibleCount.count >= filtered.length) {
+      loaderBtn.style.display = 'none';
+    } else {
+      loaderBtn.style.display = 'flex';
+    }
 
     cardsWrapper.classList.remove('fade-out');
     cardsWrapper.classList.add('fade-in');
